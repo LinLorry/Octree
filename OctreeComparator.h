9d5874f22266663f476a76c17746c4abd34058dd
LinLorry <lin864464995@163.com>
2019-09-28 16:10:41 +0800

    Update Octree Comparator.

--- OctreeComparator.h
+++ OctreeComparator.h
@@ -5,68 +5,62 @@
 
 namespace Octree
 {
-    enum Location 
+    const Result OctreeComparator(
+        const OctreeComparable<> & first, const OctreeComparable<> & second)
     {
-        outside = 0,
-        xPositive_yPositive_zPositive,
-        xPositive_yPositive_zNegative,
-        xPositive_yNegative_zNegative,
-        xPositive_yNegative_zPositive,
-        xNegative_yPositive_zPositive,
-        xNegative_yPositive_zNegative,
-        xNegative_yNegative_zNegative,
-        xNegative_yNegative_zPositive,
-        undefine
-    };
 
-    const Location OctreeComparator(
-        const OctreeComparable & first, const OctreeComparable & second)
-    {
-        if (first.type == leave && second.type == root)
-            return OctreeComparator(second, first);
-        else if (
-            (first.type == leave && second.type == leave) || 
-            (first.type == root && second.type == root)
-        ) return undefine;
+        if (first.getType() == leave)
+        {
+            if (second.getType() == root)
+                return OctreeComparator(second, first);
+            
+            if (
+                first.getLeaveX() == second.getLeaveX() &&
+                first.getLeaveY() == second.getLeaveY() &&
+                first.getLeaveZ() == second.getLeaveZ()
+            ) return equal;
+
+            return undefine;
+        }
+        
+        if (second.getType() == root) return undefine;
+
+        if (
+            first.getRootXMax() < second.getLeave().x || 
+            first.getRootXMin() > second.getLeave().x ||
+            first.getRootYMax() < second.getLeave().y || 
+            first.getRootYMin() > second.getLeave().y ||
+            first.getRootZMax() < second.getLeave().z ||
+            first.getRootZMin() > second.getLeave().z
+        ) return outside;
         else
         {
-            if (
-                first.data.root.xMax < second.data.leave.x || 
-                first.data.root.xMin > second.data.leave.x ||
-                first.data.root.yMax < second.data.leave.y || 
-                first.data.root.yMin > second.data.leave.y ||
-                first.data.root.zMax < second.data.leave.z ||
-                first.data.root.zMin > second.data.leave.z
-            ) return outside;
-            else
-            {
-                auto xTmp = (first.data.root.xMax + first.data.root.xMax) / 2;
-                auto yTmp = (first.data.root.yMax + first.data.root.yMax) / 2;
-                auto zTmp = (first.data.root.zMax + first.data.root.zMax) / 2;
+            auto xTmp = (first.getRootXMax() + first.getRootXMin()) / 2;
+            auto yTmp = (first.getRootYMax() + first.getRootYMin()) / 2;
+            auto zTmp = (first.getRootZMax() + first.getRootZMin()) / 2;
 
-                if (second.data.leave.x > xTmp)
+            if (second.getLeaveX() > xTmp)
+            {
+                if (second.getLeaveY() > yTmp)
                 {
-                    if (second.data.leave.y > yTmp)
-                    {
-                        if (second.data.leave.z > zTmp)
-                            return xPositive_yPositive_zPositive;
-                        return xPositive_yPositive_zNegative;
-                    }
-                    if (second.data.leave.z > zTmp)
-                        return xPositive_yNegative_zPositive;
-                    return xPositive_yNegative_zNegative;
+                    if (second.getLeaveZ() > zTmp)
+                        return xPositive_yPositive_zPositive;
+                    return xPositive_yPositive_zNegative;
                 }
-                if (second.data.leave.y > yTmp)
-                {
-                    if (second.data.leave.z > zTmp)
-                        return xNegative_yPositive_zPositive;
-                    return xNegative_yPositive_zNegative;
-                }
-                if (second.data.leave.z > zTmp)
-                    return xNegative_yNegative_zPositive;
-                return xNegative_yNegative_zNegative;
+                if (second.getLeaveZ() > zTmp)
+                    return xPositive_yNegative_zPositive;
+                return xPositive_yNegative_zNegative;
             }
-        }        
+            if (second.getLeaveY() > yTmp)
+            {
+                if (second.getLeaveZ() > zTmp)
+                    return xNegative_yPositive_zPositive;
+                return xNegative_yPositive_zNegative;
+            }
+            if (second.getLeaveZ() > zTmp)
+                return xNegative_yNegative_zPositive;
+            return xNegative_yNegative_zNegative;
+        } 
     }
 } // namespace Octree
 
